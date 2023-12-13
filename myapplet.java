2180c47fdf0a0e6afbe5f0b8d40d89f954a60819
vhariscmh <vharisabdul@gmail.com>
2023-12-13 23:38:13 +0530

    java

---
+++ myapplet.java
@@ -0,0 +1,13 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+public class Myapplet extends Applet
+{
+public void paint(Graphics g)
+{
+g.drawString("HELLO APPLET",200,200);
+}
+}
+/*<Applet code=Myapplet.class width=400 height=500>
+</Applet>
+*/
+
