2fef14dfa0f21ee674670d670e7f953632eb3724
vhariscmh <vharisabdul@gmail.com>
2023-12-13 23:30:12 +0530

    nase

---
+++ NASE.java
@@ -0,0 +1,18 @@
+public class NASE
+{
+public static void main(String args[])
+{
+try
+{
+int [] array=new int[-1];
+}
+Catch(NegativeArraySizeException obj)
+{
+Obj.printStackTrace();
+}
+System.out.println("Exception caught and continue execution");
+}
+}
+
+
+
